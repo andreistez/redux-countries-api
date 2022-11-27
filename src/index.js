f915df49dfd2169a60be0e94fff909535b06bb54
Andrei Stezenko <andrew.stezenko@gmail.com>
2022-11-28 01:30:04 +0300

    feat: add react-persist to save state to browser's local storage.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App'
 
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { persistor, store } from './store'
 
 ReactDOM.render(
 	<React.StrictMode>
         <Provider store={ store }>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
+			<PersistGate loading={ null } persistor={ persistor }>
+				<BrowserRouter>
+					<App/>
+				</BrowserRouter>
+			</PersistGate>
         </Provider>
 	</React.StrictMode>,
 	document.getElementById( 'root' )
